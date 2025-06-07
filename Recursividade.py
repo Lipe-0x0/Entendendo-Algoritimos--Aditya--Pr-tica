9aecb4f5d4f978f74c0564fb095635e264fe6ae3
LipeDev <152323104+Lipe-0x0@users.noreply.github.com>
2025-06-07 20:50:21 -0300

    Create Recursividade.py

---
+++ Recursividade.py
@@ -0,0 +1,9 @@
+def fat(n):
+    if n == 1: # Caso base (para finalizar a função)
+        return 1
+    else: # Caso recursivo
+        return n*fat(n-1)
+    
+# Recursividade não costuma ser bom pois trabalha com pilhas e consequentemente consome muita mémoria
+
+print(fat(10))
