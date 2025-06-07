2d6e25fffeb4d0d81ad3ef00d1b7d6871025a43b
LipeDev <152323104+Lipe-0x0@users.noreply.github.com>
2025-06-07 19:11:25 -0300

    pesquisa binaria adicionada

---
+++ Pequisa_bin.py
@@ -0,0 +1,24 @@
+def busca_bin(array, valor):
+    menor_ind = 0
+    maior_ind = len(array)-1
+    
+    while menor_ind<=maior_ind:
+        meio_ind = int((maior_ind+menor_ind)/2)
+        chute = array[meio_ind]
+        if chute == valor:
+            return meio_ind
+        elif chute > valor:
+            maior_ind = meio_ind - 1
+        else:
+            menor_ind = meio_ind + 1
+    return -1
+
+# Para verificação da eficácia, recomendo utilizar time.time
+# Ex:
+import time
+
+tempo1 = time.time()
+print(busca_bin([1,2,3,4,5,6,10,12,13,14,15,20,21,23,24,25],27))
+tempo2 = time.time()
+
+print(tempo2-tempo1)
