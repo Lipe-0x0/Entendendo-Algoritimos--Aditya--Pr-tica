de51a847ce0ae0750ec09efddde0956e4cc5d235
LipeDev <152323104+Lipe-0x0@users.noreply.github.com>
2025-06-22 23:43:24 -0300

    recursividade soma adicionada

--- Recursividade.py
+++ Recursividade.py
@@ -1,3 +1,4 @@
+# Recursividade Fatorial
 def fat(n):
     if n == 1: # Caso base (para finalizar a função)
         return 1
@@ -7,3 +8,15 @@ def fat(n):
 # Recursividade não costuma ser bom pois trabalha com pilhas e consequentemente consome muita mémoria
 
 print(fat(13))
+
+# Recursividade Soma
+def soma(arra):
+    if len(arra)==1:
+        return arra[0] # Caso base
+    else: # Caso recursivo
+        ultimo_elem = arra.pop(len(arra)-1) # Pega o ultimo elemento
+        return ultimo_elem + soma(arra) # Soma o ultimo elemento com o array modificado
+    
+ar = [2,5,7]
+
+print(soma(ar))
